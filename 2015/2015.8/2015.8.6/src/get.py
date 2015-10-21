65bf8fc8a72cc30b2bb78a04195fdad257e1a271
SCaffrey <imcaffrey@126.com>
2015-10-21 22:54:03 +0800

    pep8

--- 2015/2015.8/2015.8.6/src/get.py
+++ 2015/2015.8/2015.8.6/src/get.py
@@ -1,18 +1,26 @@
-#coding=utf-8
+# -*- coding=utf-8 -*-
 import os
 import shutil
 
 pathList = os.listdir(os.getcwd())
 for i in pathList:
-	if not(os.path.isfile(os.path.join(os.getcwd(), i))):
-		Dir = os.listdir(os.path.join(os.getcwd(), i))
-		for j in Dir:
-			try:
-				shutil.copy(os.path.join(os.getcwd(), i, j, j + '.cpp'), os.path.join(os.getcwd(), i, j + '.cpp'))
-				shutil.rmtree(os.path.join(os.getcwd(), i, j), ignore_errors = False)
-			except BaseException:
-				try:
-					shutil.copy(os.path.join(os.getcwd(), i, j, j + '.pas'), os.path.join(os.getcwd(), i, j + '.pas'))
-					shutil.rmtree(os.path.join(os.getcwd(), i, j),ignore_errors = False)
-				except BaseException:
-					print '%s %s err' % (i, j)
+    if not(os.path.isfile(os.path.join(os.getcwd(), i))):
+        Dir = os.listdir(os.path.join(os.getcwd(), i))
+        for j in Dir:
+            try:
+                shutil.copy(os.path.join(
+                    os.getcwd(), i, j, j + '.cpp'),
+                    os.path.join(os.getcwd(), i, j + '.cpp'))
+                shutil.rmtree(
+                    os.path.join(os.getcwd(), i, j),
+                    ignore_errors=False)
+            except BaseException:
+                try:
+                    shutil.copy(os.path.join(
+                        os.getcwd(), i, j, j + '.pas'),
+                        os.path.join(os.getcwd(), i, j + '.pas'))
+                    shutil.rmtree(
+                        os.path.join(os.getcwd(), i, j),
+                        ignore_errors=False)
+                except BaseException:
+                    print '%s %s err' % (i, j)
